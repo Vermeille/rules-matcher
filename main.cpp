56ae0ddd088542df0c0dccea679483e922d6affa
Guillaume 'Vermeille' Sanchez <guillaume.v.sanchez@gmail.com>
2015-11-21 04:12:42 -0800

    adapt the code for the new interface of httpi

--- main.cpp
+++ main.cpp
@@ -9,70 +9,80 @@
 
 #include <nlp-common/rules-matcher.h>
 
-#include <httpi/job.h>
+#include <httpi/html/html.h>
+#include <httpi/webjob.h>
 #include <httpi/displayer.h>
+#include <httpi/html/form-gen.h>
+#include <httpi/monitoring.h>
 
 static const unsigned int kNotFound = -1;
 
 RulesMatcher g_rm;
 
-static const JobDesc match = {
-    { { "input", "text", "Text to match" } },
+namespace htmli = httpi::html;
+
+static const htmli::FormDescriptor match_form_desc = {
     "Match",  // name
-    "/match",  // url
     "Match the input against the rules",  // longer description
-    true /* synchronous */,
-    true /* reentrant */,
-    [](const std::vector<std::string>& vs, JobResult& job) { // the actual function
-        std::istringstream input(vs[0]);
-        std::string w;
-        TrainingExample ex;
+    { { "input", "text", "Text to match" } }
+};
+
+static const std::string match_form = match_form_desc
+        .MakeForm("/match", "GET").Get();
+
+htmli::Html SeeMatches(const std::string& text) {
+    using namespace htmli;
+
+    std::istringstream input(text);
+    std::string w;
+    TrainingExample ex;
+    // FIXME: use tokenizer
+    input >> w;
+    while (input) {
+        ex.inputs.push_back({w});
         input >> w;
-        while (input) {
-            ex.inputs.push_back({0, w});
-            input >> w;
-        }
+    }
+    auto matches = g_rm.Match(ex);
 
-        Html html;
-
+    Html html;
+    if (matches.size() == 0) {
+        html << P() << "no match" << Close();
+    } else {
         html << Ul();
-        for (auto& r : g_rm.Match(ex)) {
+        for (auto& r : matches) {
             html << Li() << r << Close();
         }
         html << Close();
+    }
 
-        job.SetPage(html);
-    },
-};
+    return html;
+}
 
-static const JobDesc load = {
-    { { "rules", "file", "A file containing rules" } },
+static const htmli::FormDescriptor load_form_desc = {
     "Load",
-    "/load",
     "Load rules",
-    true /* synchronous */,
-    false /* reentrant */,
-    [](const std::vector<std::string>& vs, JobResult& job) {
-        g_rm = RulesMatcher::FromSerialized(vs[0]);
-        job.SetPage(Html() << "done");
-    },
+    { { "rules", "file", "A file containing rules" } }
 };
 
-static const JobDesc add = {
-    { { "label", "text", "The label" },
-      { "pattern", "text", "The pattern" } },
+static const std::string load_form = load_form_desc
+        .MakeForm("/model", "PUT").Get();
+
+htmli::Html CreateMatcher(const std::string& matcher) {
+    g_rm = RulesMatcher::FromSerialized(matcher);
+}
+
+static const htmli::FormDescriptor add_form_desc = {
     "Add",
-    "/Add",
     "Add rules",
-    true /* synchronous */,
-    false /* reentrant */,
-    [](const std::vector<std::string>& vs, JobResult& job) {
-        g_rm.AddRule(vs[0] + " : " + vs[1]);
-        job.SetPage(Html() << "done");
-    },
+    { { "label", "text", "The label" },
+      { "pattern", "text", "The pattern" } }
 };
 
-Html Save(const std::string&, const POSTValues&) {
+static const std::string add_form = add_form_desc
+        .MakeForm("/rules", "POST").Get();
+
+htmli::Html Save() {
+    using namespace htmli;
     return Html() << A().Id("dl").Attr("download", "bow_model.bin") << "Download Model" << Close() <<
         Tag("textarea").Id("content").Attr("style", "display: none") << g_rm.Serialize() << Close() <<
         Tag("script") <<
@@ -84,34 +94,120 @@ Html Save(const std::string&, const POSTValues&) {
         Close();
 }
 
-Html DisplayRules(const std::string&, const POSTValues&) {
+htmli::Html DisplayRules() {
+    using namespace htmli;
     Html html;
     html << H2() << "Rules" << Close();
 
     html <<
-    Tag("table").AddClass("table") <<
-        Tag("tr") <<
-            Tag("th") << "Label" << Close() <<
-            Tag("th") << "Pattern" << Close() <<
+    Table().AddClass("table") <<
+        Tr() <<
+            Th() << "Label" << Close() <<
+            Th() << "Pattern" << Close() <<
         Close();
 
     for (auto& r : g_rm) {
         html <<
-        Tag("tr") <<
-            Tag("td") << r.key() << Close() <<
-            Tag("td") << r.AsString() << Close() <<
+        Tr() <<
+            Td() << r.key() << Close() <<
+            Td() << r.AsString() << Close() <<
         Close();
     }
     html << Close();
     return html;
 }
 
+std::string MakePage(const std::string& content) {
+    return (httpi::html::Html() <<
+        "<!DOCTYPE html>"
+        "<html>"
+           "<head>"
+                R"(<meta charset="utf-8">)"
+                R"(<meta http-equiv="X-UA-Compatible" content="IE=edge">)"
+                R"(<meta name="viewport" content="width=device-width, initial-scale=1">)"
+                R"(<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">)"
+                R"(<link rel="stylesheet" href="//cdn.jsdelivr.net/chartist.js/latest/chartist.min.css">)"
+                R"(<script src="//cdn.jsdelivr.net/chartist.js/latest/chartist.min.js"></script>)"
+            "</head>"
+            "<body lang=\"en\">"
+                "<div class=\"container\">"
+                    "<div class=\"col-md-9\">" <<
+                        content <<
+                    "</div>"
+                    "<div class=\"col-md-3\">" <<
+                        "JOBS"
+                    "</div>"
+                "</div>"
+            "</body>"
+        "</html>").Get();
+}
+
 int main(int argc, char** argv) {
     InitHttpInterface();  // Init the http server
-    RegisterJob(match);
-    RegisterJob(load);
-    RegisterUrl("/rules", DisplayRules);
-    RegisterUrl("/save", Save);
+
+    WebJobsPool jp;
+    auto t1 = jp.StartJob(std::unique_ptr<MonitoringJob>(new MonitoringJob()));
+    auto monitoring_job = jp.GetId(t1);
+
+    RegisterUrl("/", [&monitoring_job](const std::string&, const POSTValues&) {
+            return MakePage(*monitoring_job->job_data().page());
+        });
+
+    RegisterUrl("/match",
+            [](const std::string& method, const POSTValues& args) {
+                using namespace httpi::html;
+                Html html;
+
+                auto vargs = match_form_desc.ValidateParams(args);
+                if (std::get<0>(vargs)) {
+                    html << P().AddClass("alert") << "No input" << Close();
+                } else {
+                    html << SeeMatches(std::get<2>(vargs)[0]);
+                }
+                html << match_form;
+                return MakePage(html.Get());
+            });
+
+    RegisterUrl("/model", [](const std::string& method, const POSTValues& args) {
+            using namespace httpi::html;
+            Html html;
+            if (method == "GET") {
+                html << Save();
+            } else if (method == "PUT") {
+                auto vargs = load_form_desc.ValidateParams(args);
+                if (std::get<0>(vargs)) {
+                    html << (std::get<1>(vargs).Get());
+                } else {
+                    CreateMatcher(std::get<2>(vargs)[0]);
+                    html << "Model loaded";
+                }
+            } else {
+                html << "no such method";
+            }
+            html << load_form;
+            return MakePage(html.Get());
+        });
+
+    RegisterUrl("/rules",
+            [](const std::string& method, const POSTValues& args) {
+                using namespace httpi::html;
+                Html html;
+
+                if (method == "POST") {
+                    auto vargs = add_form_desc.ValidateParams(args);
+                    if (std::get<0>(vargs)) {
+                        html << std::get<1>(vargs).Get();
+                    } else {
+                        auto& vs = std::get<2>(vargs);
+                        g_rm.AddRule(vs[0] + " : " + vs[1]);
+                    }
+                } else {
+                    html << "no such method";
+                }
+                html << DisplayRules().Get() << add_form;
+                return MakePage(html.Get());
+            });
+
     ServiceLoopForever();  // infinite loop ending only on SIGINT / SIGTERM / SIGKILL
     StopHttpInterface();  // clear resources
     return 0;
